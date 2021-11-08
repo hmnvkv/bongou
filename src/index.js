04bf2b8dcee0083e1bafc1004d68db6bcf5d6368
hmnvkv <manrique.hamed1610@gmail.com>
2021-11-07 18:09:06 -0600

    changes

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ const express = require('express'); /* Principal server dependency, server modul
 const app = express(); /* Real server, applicacion core */ 
 const path = require('path'); /* Dependecy to acces global route of views, applies for all systms */
 
-app.set('port', process.env.port || 4000) /* Global variable from express, asigning the port */
+app.set('port', process.env.PORT) /* Global variable from express, asigning the port */
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs'); /* View engine, to use enriched HTML */
 
 /* Middlewares: authentication stuff */
 
 /* Routes: Main page routes */
-app.use(require('./routes'));
+app.use(require('./routes/index'));
 
 /* Static Files */
 app.use(express.static(path.join(__dirname, 'public')));
